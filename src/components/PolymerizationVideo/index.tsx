d4ab956d918306a2c9b78b476920afbfcae293e2
skrillex1224 <572299072@qq.com>
2021-04-25 14:22:52 +0800

    提示消息组件

--- src/components/PolymerizationVideo/index.tsx
+++ src/components/PolymerizationVideo/index.tsx
@@ -82,7 +82,12 @@
 //             mediaRecorder.ondataavailable = e =>{
 //                 const chunk = e.data
 //                 console.log(chunk)
+//                 const aLink = document.createElement('a');
+//                 aLink.href = URL.createObjectURL(chunk);
 //
+//
+//                 aLink.target = '_blank';
+//                 aLink.click();
 //                 recordedChunks.push(chunk)
 //                 // console.log(URL.createObjectURL(chunk));
 //             }
@@ -133,32 +138,50 @@
 // }
 
 import React from "react";
+import ReactDOM from "react-dom";
 import {observer} from "mobx-react";
 import styles from './index.scss'
 import mp3 from '../../assets/testResources/467015.mp3'
 import mp4 from '../../assets/testResources/clip.mp4'
 import Preload from '../../components/Preload'
 import {CloseCircleOutlined} from '@ant-design/icons'
+import message from '../message'
 
 /*视频未加载完成前加锁*/
-const  lock  = true;
-
-/*录制结果合集*/
-const recordedChunks = [];
+let  lock  = true;
 
 /*加锁方法*/
 function lockerDetection(callback) {
-    if(lock) return ;
-    callback();
+    return ()=>{
+        if(lock) return ;
+        callback();
+    }
 }
 
-
+/**
+ * 开始录制 : 进行视频录制
+ * 暂停/继续录制  : 进行一段视频的暂停或开始
+ * 结束录制 :  停止一段视频的录制
+ *
+ * 合并视频: 将多个视频通过new Blob 进行合并
+ *
+ */
 @observer
 export default class Index extends React.Component<any, any>{
     videoRefObject : any  = React.createRef();
 
+    videoStream : MediaStream= null;
+
+    //@ts-ignore
+    videoRecorder : MediaRecorder = null;
+
     async componentDidMount(){
+        message.showMessage(30000);
         await this.getUserMedia();
+    }
+
+    state = {
+        recordedChunks : []
     }
 
     getUserMedia = async  ()=>{
@@ -166,15 +189,31 @@ export default class Index extends React.Component<any, any>{
         const rem  = parseInt(document.documentElement.style.fontSize);
         const constraints = {video : {width:{},height:{exact:  500 }}}
         const videoContext = this.videoRefObject.current;
+
         try {
             //获取视频流
             const mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
-            console.log(mediaStream)
             videoContext.srcObject = mediaStream;
+            this.videoStream = mediaStream;
+
+            //创建录制对象
+            const options = { mimeType: "video/webm; codecs=vp9" };
+            //@ts-ignore
+            const recorder = new MediaRecorder(mediaStream,options);
+            this.videoRecorder = recorder;
+            //绑定监听事件
+            this.videoRecorder.ondataavailable = (event)=>{
+                const chunk = event.data;
+                this.setState((state)=>{
+                    /*注意: 单单使用包含已经录制好媒体切片的Blobs 将大可不能单独播放. 媒体在重放之前需要重新组装 .
+                    * 所以要进行视频裁剪,但是视频剪辑又不太能写, 所以可以通过控制video标签的进度条来实现*/
+                    return {recordedChunks: [...state.recordedChunks,chunk]}
+                })
+            }
 
             videoContext.onloadedmetadata = function(){
                 this.play();
-                this.lock = false;
+                lock = false;
             }
 
         } catch (e) {
@@ -184,10 +223,49 @@ export default class Index extends React.Component<any, any>{
     }
 
     handleStart = ()=>{
+        const state =this.videoRecorder.state;
+
+        if(state === 'inactive'){
+            this.videoRecorder.start();
+            console.log('start....')
+        }
 
     }
 
+    handleResume = ()=>{
+        const state =this.videoRecorder.state;
+        if(state === 'paused'){
+            this.videoRecorder.resume();
+            console.log('resume.....')
+        }
+
+    }
+
+    handlePause = ()=>{
+        const state =this.videoRecorder.state;
+        if(state === 'recording'){
+            this.videoRecorder.pause();
+            //调用以下方法 将会截取一段ondataavailable
+            // this.videoRecorder.requestData();
+            console.log('pause.....')
+        }
+    }
+
+    handleStop = ()=>{
+        const state =this.videoRecorder.state;
+        if(state === 'recording'){
+            this.videoRecorder.stop();
+            console.log('stop....')
+        }
+    }
+
+    mergeVideo =()=>{
+        const {recordedChunks} = this.state;
+        console.log(URL.createObjectURL(new Blob(recordedChunks, { 'type' : 'video/webm' })))
+    }
+
     render(): React.ReactNode {
+        const {recordedChunks} = this.state;
         return (
                 <div className={styles.wrapper} >
                     <audio  id='my-audio' src={mp3} />
@@ -197,20 +275,22 @@ export default class Index extends React.Component<any, any>{
                     <div className={styles.wrapper_chunks}>
 
                         {
-                            [1,1,1,1,,1,1,1,1,1,1,1,1,1,1,1,1,1,].map(()=>(
-                                <div className={styles.wrapper_chunks_videoItem}>
-                                    <div className={styles.wrapper_chunks_videoItem_delete}><CloseCircleOutlined /></div>
-                                    <video className={styles.wrapper_chunks_videoItem_videoContent} controls={true} src={'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4'} />
-                                 </div>
-                            ))
+                            recordedChunks.map((item,index)=> {
+                                //释放上一次的资源
+                                URL.revokeObjectURL(item);
+                                return (<div key={index} className={styles.wrapper_chunks_videoItem}>
+                                    <div className={styles.wrapper_chunks_videoItem_delete}><CloseCircleOutlined/></div>
+                                    <video className={styles.wrapper_chunks_videoItem_videoContent} controls={true} src={URL.createObjectURL(item)}/>
+                                </div>)
+                            })
                         }
                     </div>
                     <div className={styles.wrapper_ops}>
-                        <div onClick={this.handleStart} className={styles.wrapper_ops_btn}>开始录制</div>
-                        <div className={styles.wrapper_ops_btn}>暂停录制</div>
-                        <div className={styles.wrapper_ops_btn}>继续录制</div>
-                        <div className={styles.wrapper_ops_btn}>结束录制</div>
-                        <div className={styles.wrapper_ops_btn}>插入音乐</div>
+                        <div onClick={lockerDetection(this.handleStart)} className={styles.wrapper_ops_btn}>开始录制</div>
+                        <div onClick={lockerDetection(this.handleResume)}  className={styles.wrapper_ops_btn}>继续录制</div>
+                        <div onClick={lockerDetection(this.handlePause)}  className={styles.wrapper_ops_btn}>暂停录制</div>
+                        <div onClick={lockerDetection(this.handleStop)}  className={styles.wrapper_ops_btn}>结束录制</div>
+                        <div onClick={this.mergeVideo} className={styles.wrapper_ops_btn}>合并录制结果</div>
                     </div>
                 </div>
         )
---
+++ src/components/message/index.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import {observer} from "mobx-react";
+import styles from './index.scss'
+import ReactDOM from "react-dom";
+import {CheckCircleOutlined, CloseCircleOutlined, ExclamationCircleOutlined} from "@ant-design/icons/lib";
+
+interface IProps {
+    message : string,
+    icon :string
+}
+
+interface IState {
+    visible : boolean
+}
+
+/**
+ *
+ * 使用方法同antd的message对象
+ */
+@observer
+export default class Index extends React.Component<IProps, IState>{
+    static defaultProps = {
+        message : '提示消息',
+        icon:'warn'
+    }
+
+    private  static duration = 3000;
+
+    private static typeMapping = {
+        'warn' : <ExclamationCircleOutlined />,
+        'success' : <CheckCircleOutlined />,
+        'error' : <CloseCircleOutlined />,
+    }
+
+    public  static showMessage = (duration = 3000)=>{
+        Index.duration = duration;
+        const ele = <Index />
+        const divEle = document.createElement('div');
+        divEle.id = 'message';
+        divEle.style.position = 'absolute'
+        divEle.style.left = '0'
+        divEle.style.top = '0'
+        divEle.style.height ='60px';
+        divEle.style.width ='100%';
+        divEle.style.zIndex = '999';
+
+        document.body.append(divEle);
+
+        //从页面上获取node
+        const divRenderedEle = document.getElementById('message');
+        //@ts-ignore
+        ReactDOM.render(ele, divRenderedEle);
+        setTimeout(()=>{
+            //从页面上获取node
+            ReactDOM.unmountComponentAtNode(divRenderedEle);
+            document.body.removeChild(divRenderedEle);
+        },duration  + 1100)
+    }
+
+
+    state = {
+        visible : false
+    }
+
+    componentDidMount(): void {
+        setTimeout(()=>{
+            this.setState({visible:true})
+            setTimeout(()=>{
+                this.setState({visible:false})
+            },Index.duration )
+        })
+    }
+
+    render() {
+        const {visible} = this.state;
+        const {icon,message} = this.props;
+        return (
+            <div style={visible  ? {transform:'translateY(20px)'} : {}} className={styles.wrapper}>
+                <div className={styles.wrapper_main}>
+                    <span className={styles.wrapper_main_icon}>{Index.typeMapping[icon]}</span>
+                    <span>{message}</span>
+                </div>
+            </div>
+        )
+    }
+}

da6a896eac3fd71c68b2cd671a281c822b021d90
572299072 <572299072@qq.com>
2021-04-28 16:45:40 +0800

    将图片放在另一张图片上

--- src/pages/CaseTestSet/WaterMark/index.tsx
+++ src/pages/CaseTestSet/WaterMark/index.tsx
@@ -20,113 +20,96 @@ import {makeObservable, observable} from "mobx";
 
 @observer
 export default class Index extends React.Component<any, any>{
-    constructor(props) {
-        super(props);
-        makeObservable(this)
-    }
+    //当拖拽开始时,鼠标相对于图片的偏移
+     onDragStartOffsets = {
+         offsetX:0,
+         offsetY:0
+     }
 
-    //当前整个scrollBar 平移的距离
-    @observable
-    translateX : number =  0
-
-    //节流 并判断是否在点击, 同设置所有图片是否可拖动, 当拖动大框滑动时禁用拖动
-    @observable
-    isMouseDown = false;
-
-    // 记录鼠标按下时的clientX
-    onMouseDownX = 0;
-
-    //记录鼠标按下时的translateX
-    onMouseDownTranslateX = 0;
-
-
-    //滚动的div
-    scrollContainer :any  = React.createRef();
-
-    //包裹滚动div的wrapper
-    scrollWrapper : any = React.createRef();
-
-    state = {
-        srcList : [],
-    }
-
+     state = {
+         srcList :[],
+         dragEntered : false,
+     }
     componentDidMount(): void {
-        const srcList = [];
-        for (let i = 0; i < 20; i++) {
+         const srcList = this.state.srcList;
+        for (let i = 0; i < 6; i++) {
             srcList.push(reactLogo);
         }
         this.setState({srcList});
-
     }
 
-    //节流
-    throttle = (eventHandler : Function,duration = 300)=>{
-        let timer ;
-        return (e)=>{
-            if(timer || !this.isMouseDown) return ;
-            timer = setTimeout(()=>{
-                eventHandler(e)
-                timer =  null;
-            },duration)
+    handleDragEnter = (e)=>{
+        this.setState({dragEntered:true})
+        e.preventDefault();
+    }
+
+    handleDragLeave = (e)=>{
+        this.setState({dragEntered:false})
+        e.preventDefault();
+    }
+
+    handleDragOver = (e)=>{
+        e.preventDefault();
+    }
+
+    handleDrop = (e)=>{
+         this.setState({dragEntered:false})
+        //获取鼠标的最后位置
+         const offsetX = e.nativeEvent.offsetX;
+         const offsetY = e.nativeEvent.offsetY;
+
+         const img = new Image(50,50);
+         //获取src
+         img.src = e.dataTransfer.getData('image/png');
+
+         //根据初始偏移和现在鼠标的位置，计算图片真正该放置的位置
+         let trueX = offsetX - this.onDragStartOffsets.offsetX
+         let trueY = offsetY - this.onDragStartOffsets.offsetY;
+
+         //防止越界
+        console.log(trueX,trueY)
+        if(trueX <0) trueX = 0;
+        if(trueY <0) trueY = 0;
+
+        img.style.position ='absolute';
+        img.style.left = `${trueX}px`;
+        img.style.top = `${trueY}px` ;
+        img.style.zIndex = '999';
+
+        const imgContainer = document.getElementById('img-container');
+        imgContainer.append(img);
+
+        e.preventDefault();
+    }
+
+    handleDragStart = (e)=>{
+        const offsetX = e.nativeEvent.offsetX;
+        const offsetY = e.nativeEvent.offsetY;
+        //设置初始偏移
+        this.onDragStartOffsets = {
+            offsetX,
+            offsetY
         }
+
+        //设置数据
+         e.dataTransfer.setData('image/png',reactLogo)
     }
-
-     handleScrollStart = (e)=>{
-        //记录按下时
-         const x = e.clientX;
-         this.onMouseDownX =  x;
-         this.onMouseDownTranslateX = this.translateX;
-
-        //设置鼠标被按下
-        this.isMouseDown = true;
-        e.preventDefault()
-         console.log('dsadasd')
-    }
-
-    handleScrolling = (e)=>{
-        const x = e.clientX;
-        //和初始位置的差值,即要改变的值
-        let minx = x - this.onMouseDownX
-
-
-        console.log(this.onMouseDownTranslateX +  minx,  )
-        //防止越界
-        //这个值this.onMouseDownTranslateX +  minx  就是 改变后的translateX , 所以判定这个值
-        if(this.onMouseDownTranslateX +  minx > 0) return ;
-        if(Math.abs(this.onMouseDownTranslateX +  minx) - 10 > this.scrollContainer.current.scrollWidth  - this.scrollWrapper.current.clientWidth) return ;
-
-
-
-        this.translateX = this.onMouseDownTranslateX +  minx;
-        e.preventDefault()
-    }
-
-    /*将up事件绑定给最外层的,保证全部拖动有效*/
-    handleScrollEnd = (e)=>{
-        this.isMouseDown = false;
-        e.preventDefault()
-        console.log('up')
-    }
-
-    preventDefaultMethod = (e)=>e.preventDefault();
 
     render(): React.ReactNode {
-        const {srcList,} = this.state;
+        const {srcList,dragEntered} = this.state;
         return (
-            <div onMouseUp={this.handleScrollEnd}  className={styles.wrapper}>
-                <img  className={styles.wrapper_imgContainer} src={markBg} />
+            <div  className={styles.wrapper}>
+                <div id={'img-container'} onDragEnter={this.handleDragEnter} onDragOver={this.handleDragOver} onDragLeave={this.handleDragLeave} onDrop={this.handleDrop}
+                     style={dragEntered ?{border:'2px dashed #fffa',filter:'blur(1px) opacity(.8)'} : {}}  className={styles.wrapper_imgContainer} >
+                    <img className={styles.wrapper_imgContainer_imgBg} src={markBg}/>
+                </div>
                 <div className={styles.wrapper_drawWrapper}>
                     {/*overflow:hidden 如果设置内边距,内边距上的内容依旧会显示,所以为了有padding效果,加格这个  https://segmentfault.com/q/1010000021907068*/}
                     {/*禁用默认行为一禁用就要禁用一整套方法*/}
-                    <div ref={this.scrollWrapper} onMouseMove={this.throttle(this.handleScrolling,10)}  onMouseDown={this.handleScrollStart}
-                         onMouseUp={this.preventDefaultMethod} className={styles.wrapper_drawWrapper_padding}>
-                        <div style={{transform:`translateX(${this.translateX}px)`}} ref={this.scrollContainer}
-                                className={styles.wrapper_drawWrapper_drawList} >
-                            {srcList.map((item,index)=>(
-                                <img onDrag={()=>alert('sadasd')}  draggable  src={item}  className={styles.wrapper_drawWrapper_drawList_waterMark} key={index} />
-                            ))}
-                        </div>
-                    </div>
+                    {srcList.map((item,index)=>(
+                        <img onDragStart={this.handleDragStart}
+                             draggable  src={item}  className={styles.wrapper_drawWrapper_waterMark} key={index} />
+                    ))}
                 </div>
             </div>
         )

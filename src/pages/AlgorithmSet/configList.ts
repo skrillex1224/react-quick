7bfd2d5cfae6ab2c7c00461c5d1b15a31ad0e4df
572299072 <572299072@qq.com>
2021-04-29 16:37:29 +0800

    修改瀑布流

---
+++ src/pages/AlgorithmSet/configList.ts
@@ -0,0 +1,428 @@
+export default [
+    1,2,3,4,5,6,7,2,13,213,12,31,3,123,12,321,3,12,321,3,123,21,312,312,1,1,1,1,1,1,1,1,1,1,1,
+]
+
+export const code =
+`
+function createModule(str1, str2) {
+        function Obj(){
+            this.greeting = str1;
+            this.name = str2;
+            this.sayIt = function(){
+                return this.greeting + ', ' + this.name;
+            };
+        }
+        return new Obj();
+    }
+    //构造函数与原型组合
+    function createModule(str1, str2) {
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+    //构造函数与原型组合
+    function createModule(str1, str2) {                                 //构造函数与原型组合
+    function createModule(str1, str2) {
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+function createModule(str1, str2) {
+        function Obj(){
+            this.greeting = str1;
+            this.name = str2;
+            this.sayIt = function(){
+                return this.greeting + ', ' + this.name;
+            };
+        }
+        return new Obj();
+    }
+    //构造函数与原型组合
+    function createModule(str1, str2) {
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+    //构造函数与原型组合
+    function createModule(str1, str2) {                                 //构造函数与原型组合
+    function createModule(str1, str2) {
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+function createModule(str1, str2) {
+        function Obj(){
+            this.greeting = str1;
+            this.name = str2;
+            this.sayIt = function(){
+                return this.greeting + ', ' + this.name;
+            };
+        }
+        return new Obj();
+    }
+    //构造函数与原型组合
+    function createModule(str1, str2) {
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+    //构造函数与原型组合
+    function createModule(str1, str2) {                                 //构造函数与原型组合
+    function createModule(str1, str2) {
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+function createModule(str1, str2) {
+        function Obj(){
+            this.greeting = str1;
+            this.name = str2;
+            this.sayIt = function(){
+                return this.greeting + ', ' + this.name;
+            };
+        }
+        return new Obj();
+    }
+    //构造函数与原型组合
+    function createModule(str1, str2) {
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+    //构造函数与原型组合
+    function createModule(str1, str2) {                                 //构造函数与原型组合
+    function createModule(str1, str2) {
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+function createModule(str1, str2) {
+        function Obj(){
+            this.greeting = str1;
+            this.name = str2;
+            this.sayIt = function(){
+                return this.greeting + ', ' + this.name;
+            };
+        }
+        return new Obj();
+    }
+    //构造函数与原型组合
+    function createModule(str1, str2) {
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+    //构造函数与原型组合
+    function createModule(str1, str2) {                                 //构造函数与原型组合
+    function createModule(str1, str2) {
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+function createModule(str1, str2) {
+        function Obj(){
+            this.greeting = str1;
+            this.name = str2;
+            this.sayIt = function(){
+                return this.greeting + ', ' + this.name;
+            };
+        }
+        return new Obj();
+    }
+    //构造函数与原型组合
+    function createModule(str1, str2) {
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+    //构造函数与原型组合
+    function createModule(str1, str2) {                                 //构造函数与原型组合
+    function createModule(str1, str2) {
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+function createModule(str1, str2) {
+        function Obj(){
+            this.greeting = str1;
+            this.name = str2;
+            this.sayIt = function(){
+                return this.greeting + ', ' + this.name;
+            };
+        }
+        return new Obj();
+    }
+    //构造函数与原型组合
+    function createModule(str1, str2) {
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+    //构造函数与原型组合
+    function createModule(str1, str2) {                                 //构造函数与原型组合
+    function createModule(str1, str2) {
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+function createModule(str1, str2) {
+        function Obj(){
+            this.greeting = str1;
+            this.name = str2;
+            this.sayIt = function(){
+                return this.greeting + ', ' + this.name;
+            };
+        }
+        return new Obj();
+    }
+    //构造函数与原型组合
+    function createModule(str1, str2) {
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+    //构造函数与原型组合
+    function createModule(str1, str2) {                                 //构造函数与原型组合
+    function createModule(str1, str2) {
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+function createModule(str1, str2) {
+        function Obj(){
+            this.greeting = str1;
+            this.name = str2;
+            this.sayIt = function(){
+                return this.greeting + ', ' + this.name;
+            };
+        }
+        return new Obj();
+    }
+    //构造函数与原型组合
+    function createModule(str1, str2) {
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+    //构造函数与原型组合
+    function createModule(str1, str2) {                                 //构造函数与原型组合
+    function createModule(str1, str2) {
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+function createModule(str1, str2) {
+        function Obj(){
+            this.greeting = str1;
+            this.name = str2;
+            this.sayIt = function(){
+                return this.greeting + ', ' + this.name;
+            };
+        }
+        return new Obj();
+    }
+    //构造函数与原型组合
+    function createModule(str1, str2) {
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+    //构造函数与原型组合
+    function createModule(str1, str2) {                                 //构造函数与原型组合
+    function createModule(str1, str2) {
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+
+`
--- src/pages/AlgorithmSet/index.tsx
+++ src/pages/AlgorithmSet/index.tsx
@@ -5,31 +5,53 @@ import {Animated} from 'react-animated-css'
 import MainScreen from "../../components/MainScreen";
 import IndexSet from "../../utils/IndexSet";
 import SubjectBox from './SubjectBox'
+import algorithmList, {code} from './configList'
 
 import styles from './index.scss'
+import classNames from "classnames";
 
 /*本屏幕的下标*/
 const IndexSetOfThisIndex = 2;
 
 const hiddenTime =600;
 
-const algorithmList = [
-    1,2,3,4,5,6,7,8,
-]
 
 @observer
 export default class  Index extends React.Component<any, any>{
 
+    column1 : any = React.createRef();
+    column2 : any = React.createRef();
+    column3 : any = React.createRef();
+
     state = {
         isLoading :true,
         isVisible : true,
+        colOneList : [],
+        colTwoList : [],
+        colThreeList : [],
     }
 
 
     componentDidMount(): void {
-        setTimeout(()=>{
+        // setTimeout(()=>{
             this.setState({isLoading:false})
-        },600)
+        // },600)
+
+        this.initializeAlgorithmList();
+            console.log(this.state)
+    }
+
+    /*初始化列表,先将三列填满后再比较高低,然后再动态进行平衡,这样可以避免多次setState()*/
+    initializeAlgorithmList = ()=>{
+        const {colThreeList,colTwoList,colOneList} = this.state;
+        for (let i = 0; i < algorithmList.length; i++) {
+            i % 3 === 0  &&  colOneList.push(algorithmList[i]);
+            i % 3 === 1  &&  colTwoList.push(algorithmList[i]);
+            i % 3 === 2  &&   colThreeList.push(algorithmList[i]);
+        }
+
+        this.setState({colThreeList,colTwoList,colOneList})
+
     }
 
 
@@ -41,8 +63,9 @@ export default class  Index extends React.Component<any, any>{
     }
 
 
+
     render() {
-        const {isLoading,isVisible} =this.state;
+        const {isLoading,isVisible,colTwoList,colOneList,colThreeList} =this.state;
         const {title,subTitle,imgName} = IndexSet[IndexSetOfThisIndex];
 
     /**animationOut没啥用在这里*/
@@ -52,14 +75,19 @@ export default class  Index extends React.Component<any, any>{
                          animationOutDuration={hiddenTime}>
                    <MainScreen title={title} subTitle={subTitle} imgName={imgName}/>
                </Animated>
-               <div className={styles.wrapper}>
-                   {
-                       algorithmList.map((item,index)=>(
-                           <SubjectBox title={'获取URL'} index={1} desc={'完成函数 createModule，调用之后满足如下要求：\n' +
-                           '1、返回一个对象\n' +
-                           '2、对象的 greeting 属性值等于 str1， name 属性值等于 str2\n' +
-                           '3、对象存在一个 sayIt 方法，该方法返回的字符串为 greeting属性值 + \', \' + name属性值'}
-                                       code={`
+                <div className={styles.wrapper}>
+                    <div ref={this.column1} className={classNames({
+                        [`${styles.wrapper_column1}`]: true,
+                        [`${styles.wrapper_column}`]: true,
+                    })}>{
+                        colOneList.map((item,index)=>(
+                            <Animated   animationIn={'rollIn'} animationOut={'fadeOut'} isVisible={true} animationInDuration={1600}
+                                        animationOutDuration={hiddenTime} >
+                                <SubjectBox title={'获取URL'} index={index} desc={'完成函数 createModule，调用之后满足如下要求：\n' +
+                                '1、返回一个对象\n' +
+                                '2、对象的 greeting 属性值等于 str1， name 属性值等于 str2\n' +
+                                '3、对象存在一个 sayIt 方法，该方法返回的字符串为 greeting属性值 + \', \' + name属性值'}
+                                            code={`
 function createModule(str1, str2) {
         function Obj(){
             this.greeting = str1;
@@ -103,10 +131,85 @@ function createModule(str1, str2) {
         return new CreateMod();
     }
 
-`}/>
-                       ))
-                   }
-               </div>
+`.slice(0,index * 820)}/>
+                            </Animated>
+                        ))
+                    }</div>
+                    <div ref={this.column2} className={classNames({
+                        [`${styles.wrapper_column2}`]: true,
+                        [`${styles.wrapper_column}`]: true,
+                    })}>{
+                        colOneList.map((item,index)=>(
+                            <Animated   animationIn={'rollIn'} animationOut={'fadeOut'} isVisible={true} animationInDuration={1600}
+                                        animationOutDuration={hiddenTime} >
+                                <SubjectBox title={'获取URL'} index={index} desc={'完成函数 createModule，调用之后满足如下要求：\n' +
+                                '1、返回一个对象\n' +
+                                '2、对象的 greeting 属性值等于 str1， name 属性值等于 str2\n' +
+                                '3、对象存在一个 sayIt 方法，该方法返回的字符串为 greeting属性值 + \', \' + name属性值'}
+                                            code={code.slice(0,index * 300)}/>
+                            </Animated>
+                        ))
+                    } </div>
+                    <div ref={this.column3} className={classNames({
+                        [`${styles.wrapper_column2}`]: true,
+                        [`${styles.wrapper_column}`]: true,
+                    })}>{
+                        colOneList.map((item,index)=>(
+                            <Animated   animationIn={'rollIn'} animationOut={'fadeOut'} isVisible={true} animationInDuration={1600}
+                                        animationOutDuration={hiddenTime} >
+                                <SubjectBox title={'获取URL'} index={index} desc={'完成函数 createModule，调用之后满足如下要求：\n' +
+                                '1、返回一个对象\n' +
+                                '2、对象的 greeting 属性值等于 str1， name 属性值等于 str2\n' +
+                                '3、对象存在一个 sayIt 方法，该方法返回的字符串为 greeting属性值 + \', \' + name属性值'}
+                                            code={`
+function createModule(str1, str2) {
+        function Obj(){
+            this.greeting = str1;
+            this.name = str2;
+            this.sayIt = function(){
+                return this.greeting + ', ' + this.name;
+            };
+        }
+        return new Obj();
+    }
+    //构造函数与原型组合
+    function createModule(str1, str2) {
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+    //构造函数与原型组合
+    function createModule(str1, str2) {                                 //构造函数与原型组合
+    function createModule(str1, str2) {
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+        function CreateMod(){
+            this.greeting = str1;
+            this.name = str2;
+        }
+        CreateMod.prototype.sayIt = function(){
+            return this.greeting + ', '  + this.name;
+        }
+        return new CreateMod();
+    }
+
+`.slice(0,index * 160)}/>
+                            </Animated>
+                        ))
+                    } </div>
+                </div>
             </Preload>
         )
     }
